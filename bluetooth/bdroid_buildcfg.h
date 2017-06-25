46127ac0ff2ad28604d8cdf2ab6120758a1bd1fc
Luca Stefani <luca020400@lineageos.org>
2017-06-25 12:56:07 +0200

    sdm439-common: Append 'Xiaomi' to BT name
    Change-Id: Ic01e2aa8ee69e41e8a3c0fecd15e122b44a2fed1

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -21,6 +21,29 @@
 
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
+
+#include <cutils/properties.h>
+#include <string.h>
+
+static inline const char* BtmGetDefaultName()
+{
+    char product_device[PROPERTY_VALUE_MAX];
+    property_get("ro.product.device", product_device, "");
+
+    if (strstr(product_device, "olive"))
+        return "Xiaomi Redmi 8";
+    if (strstr(product_device, "olivelite"))
+        return "Xiaomi Redmi 8A";
+    if (strstr(product_device, "olivewood"))
+        return "Xiaomi Redmi 8A Dual";
+    if (strstr(product_device, "pine"))
+        return "Xiaomi Redmi 7A";
+
+    // Fallback to ro.product.model
+    return "";
+}
+
+#define BTM_DEF_LOCAL_NAME BtmGetDefaultName()
 // Disables read remote device feature
 #define MAX_ACL_CONNECTIONS   16
 #define MAX_L2CAP_CHANNELS    16
